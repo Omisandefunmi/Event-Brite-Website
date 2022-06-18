a1992b13fa2db299d45d2d107ab07c5a484b9f56
Omisandefunmi <omisandefunmi@gmail.com>
2022-06-18 22:36:47 +0100

    added login page

---
+++ objects.js
@@ -0,0 +1,14 @@
+let userInput = {
+    firstName: "jonathan",
+    lastName: "keys",
+    getFullName: function(){
+        return this.firstName+ " "+ this.lastName
+    },
+    createUserName: function(){
+        console.log("@"+ this.firstName.charAt(0)
+        + this.lastName.charAt(this.lastName.length - 1))
+
+    }
+} 
+
+console.log(userInput.getFullName)
---
+++ submit.js
@@ -0,0 +1,7 @@
+function submit(){
+    let email = document.getElementById("email").value
+    let password = document.getElementById("placeholder").value
+    console.log(email);
+    console.log(password)
+    
+}
